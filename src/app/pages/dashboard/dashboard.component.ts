6e2a858843320ef556a2cfe75403095d563e666d
levantinhm2m <tinh.lv@m2m-sol.co.jp>
2023-11-16 14:47:06 +0700

    s

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -13,27 +13,60 @@ import { SystemService } from 'src/app/services/system.service';
 export class DashboardComponent implements OnDestroy{
   isStartDrying = false
 
-  timeDry:any = 0
-  tempDry:any = 0
+  timeDry:any = null
+  tempDry:any = null
   sensorsValue: any = null
   progress = 0;
   timeCountDown = 0;
+  isDisabledButtonStart = false
+  countdownInterval: any;
+  timeDisplay = ''
+  system: any
+
   constructor(private systemService: SystemService,private sensorService: SensorService, private authService: AuthService) {
 
     this.systemService.getStatus().subscribe((value) => {
       console.log(value);
+      this.system = value
       if (value?.[1]?.value) {
         this.isStartDrying = true;
-        this.timeCountDown = value[3]?.value;
+        this.isDisabledButtonStart = true;
+        this.timeCountDown = new Date(value[3]?.value).getTime();
       } else {
         this.isStartDrying = false;
+        this.isDisabledButtonStart = false
       }
+
+      this.startCountdown();
     });
 
     this.sensorService.getStatus().subscribe((value) => {
       console.log(value)
       this.sensorsValue = value
     })
+  }
+
+  startCountdown() {
+    this.countdownInterval = setInterval(() => {
+      const currentTime = new Date().getTime();
+      const timeDifference = this.timeCountDown - currentTime;
+
+      if (timeDifference <= 0) {
+        this.endDrySystem()
+        clearInterval(this.countdownInterval);
+      } else {
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+        // Format the time display
+        this.timeDisplay = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+      }
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.countdownInterval);
   }
 
   startDrySystem() {
@@ -43,7 +76,9 @@ export class DashboardComponent implements OnDestroy{
       return;
     }
 
-      this.systemService.startSystem({ timeDry: this.timeDry, tempDry: this.tempDry }).then(() => {
+      const time = new Date().getTime() + 1000*this.timeDry*60
+
+      this.systemService.startSystem({ timeDry: time, tempDry: this.tempDry }).then(() => {
         this.isStartDrying = true;
       });
   }
@@ -60,6 +95,4 @@ export class DashboardComponent implements OnDestroy{
     })
   }
 
-  ngOnDestroy(): void {
-  }
 }
