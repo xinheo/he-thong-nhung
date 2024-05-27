2a3a14b6a6eb05bc42b0e36d2cc9a6d3fd66bc2c
Tan Nguyen <vantan19no1@gmail.com>
2024-05-27 10:54:51 +0700

    change enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyACE3667Ows33gC1RwqKQ8_9xwAPgQZhNU",
-  authDomain: "pbl3-45657.firebaseapp.com",
-  databaseURL: "https://pbl3-45657-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "pbl3-45657",
-  storageBucket: "pbl3-45657.appspot.com",
-  messagingSenderId: "1080996007204",
-  appId: "1:1080996007204:web:0c23bd5d41acbe7a7d37b1",
-  measurementId: "G-WRWE5PNLBF"
+    apiKey: "AIzaSyDTovmX_Q2nQlC1f11vWI-1bZRKzWR2t3U",
+    authDomain: "esp32-e6121.firebaseapp.com",
+    databaseURL: "https://esp32-e6121-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "esp32-e6121",
+    storageBucket: "esp32-e6121.appspot.com",
+    messagingSenderId: "234276674276",
+    appId: "1:234276674276:web:52922043dfe3abbcc5aaf1",
+    measurementId: "G-K405XC93WS"
   }
 };
